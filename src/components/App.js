0c83a0bbaee74f95855268e8e951ddf2b8f72d32
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2021-02-16 11:49:02 +1100

    change title

--- src/components/App.js
+++ src/components/App.js
@@ -21,6 +21,8 @@ class App extends Component {
     if (this.state.query) {
       this.handleSubmit(this.state.query);
     }
+
+    document.title = "Weather App";
   }
 
   handleSubmit = query => {
@@ -35,62 +37,6 @@ class App extends Component {
         )
         .catch(error => this.setState({ error: error, loading: false }));
     });
-    // this.setState(() => ({searchTerm: query}));
-    // getLatLang(query)
-    //   .then((locationData) => {
-    //     if (locationData) {
-    //       this.setState(() => ({ location: locationData.name }))
-    //       getWeather(locationData)
-    //         .then(({ data }) => this.setState(() => {
-    //           const { currently } = data;
-    //           const hourly = data.hourly.data
-    //             .slice(0, 7).map((hourly) => ({
-    //               time: hourly.time,
-    //               icon: hourly.icon,
-    //               temperature: hourly.temperature.toFixed(1),
-    //               rain: Math.round(hourly.precipIntensity * 100) / 100,
-    //               windSpeed: hourly.windSpeed.toFixed(1),
-    //               humidity: Math.floor(hourly.humidity * 100)
-    //             }))
-    //           const daily = data.daily.data
-    //             .slice(0, 7).map((daily) => ({
-    //               time: daily.time,
-    //               icon: daily.icon,
-    //               temperature: daily.temperatureHigh.toFixed(1),
-    //               humidity: Math.floor(daily.humidity * 100),
-    //               rain: Math.round(daily.precipIntensity * 100) / 100,
-    //               sunrise: daily.sunriseTime,
-    //               sunset: daily.sunsetTime,
-    //               windSpeed: daily.windSpeed.toFixed(1),
-    //               summary: daily.summary
-    //             }))
-    //           return {
-    //             currently: {
-    //               time: currently.time,
-    //               temperature: currently.temperature.toFixed(1),
-    //               icon: currently.icon,
-    //               windSpeed: currently.windSpeed.toFixed(1),
-    //               humidity: Math.floor(currently.humidity * 100),
-    //               rain: Math.round(currently.precipIntensity * 100) / 100,
-    //               summary: data.hourly.summary,
-    //               sunrise: data.daily.data[0].sunriseTime,
-    //               sunset: data.daily.data[0].sunsetTime
-    //             },
-    //             hourly,
-    //             daily,
-    //           }
-    //         }))
-    //     } else {
-    //       this.setState(() => ({
-    //         location: '',
-    //         error: "Please enter a valid location",
-    //         currently: '',
-    //         hourly: '',
-    //         daily: '',
-    //         loading: false
-    //       }))
-    //     }
-    //   })
   };
 
   handleUnitChange = event => {
